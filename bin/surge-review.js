91fd40e4d432b10802a00271ac8feb46112a3faf
cdaringe <christopher.dieringer@gmail.com>
2017-12-19 16:34:37 -0800

    fix(config): extract sensible defaults from ci

--- bin/surge-review.js
+++ bin/surge-review.js
@@ -10,7 +10,10 @@ var path = require('path')
 
 if (argv.p || argv['publish-dir']) config.PUBLISH_DIR = path.resolve(process.cwd(), argv.p || argv['publish-dir'])
 if (argv.b || argv['pr-branch']) config.GH_PULL_BRANCH = argv.b || argv['pr-branch']
-if (argv['pull-request']) config.GH_PULL_REQUEST = argv['pull-request']
+if (argv['pull-request']) {
+  // allow slop in the pull request id
+  config.GH_PULL_REQUEST = argv['pull-request'].trim().match(/(\d+)$/)[1]
+}
 
 sr.run(config, function (err) {
   if (err) throw err
